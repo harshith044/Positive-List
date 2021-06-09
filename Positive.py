fde84d39ef45a696e64ffdb5c5cbb0c767834276
harshith044 <80192037+harshith044@users.noreply.github.com>
2021-06-09 21:56:49 +0530

    Add files via upload

---
+++ Positive.py
@@ -0,0 +1,13 @@
+mylist1=[12,-7,5,64,-14];
+num =0
+while(num<len(mylist1)):
+    if mylist1[num]>0:
+        print(mylist1[num],end=" ");
+    num+=1;
+
+mylist2=[12, 14, -95, 3];
+num=0
+while(num<len(mylist2)):
+    if mylist2[num]>0:
+        print(mylist2[num],end=" ");
+    num+=1;
